3118ef20eb76e7033b4e0c645b712c6837018398
Swaraj Dash <swarajdash40@gmail.com>
2020-07-18 08:04:08 +0530

    Set onClickListener

--- app/src/main/java/com/example/buttoncounterapp/MainActivity.kt
+++ app/src/main/java/com/example/buttoncounterapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.buttoncounterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private var userInput: EditText? = null
     private var button: Button? = null
     private var textView: TextView? = null
+    private var num = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +20,12 @@ class MainActivity : AppCompatActivity() {
         userInput = findViewById<EditText>(R.id.editText)
         button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
+
+        button?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                num += 1
+                textView?.append("\nThe button got tapped $num times")
+            }
+        })
     }
 }
